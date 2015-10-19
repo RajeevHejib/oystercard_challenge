6cb3e53c29de10a66f39518cd1fa96cbe295969b
dearshrewdwit <dearshrewdwit@gmail.com>
2015-10-19 16:07:21 +0100

    Added default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
+  it "balance has a default of 0" do
+    expect(oystercard.balance).to eq 0
+  end
+
 end
