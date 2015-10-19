19e090eb5882fd0103941b8b47b65e8c9a1091f1
dearshrewdwit <dearshrewdwit@gmail.com>
2015-10-19 18:00:18 +0100

    touch in and touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,7 @@ class Oystercard
 
   def initialize(balance=DEFAULT_BALANCE)
     @balance = balance
+    @in_use = false
   end
 
   def top_up(value)
@@ -14,6 +15,18 @@ class Oystercard
 
   def deduct(fare)
     @balance -= fare
+  end
+
+  def touch_in
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
+  end
+
+  def in_journey?
+    @in_use
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,22 +4,41 @@ describe Oystercard do
 
   subject(:oystercard) { described_class.new }
 
-  it "balance has a default of 0" do
-    expect(oystercard.balance).to eq described_class::DEFAULT_BALANCE
+  context "Balance" do
+
+    it "Has a default of 0" do
+      expect(oystercard.balance).to eq described_class::DEFAULT_BALANCE
+    end
+
+    it "Tops up the balance by a specified amount" do
+      expect{ oystercard.top_up(10) }.to change{ oystercard.balance }.by(10)
+    end
+
+    it "raises an error if exceeds #MAX_BALANCE" do
+      max_balance = described_class::MAX_BALANCE
+      expect{ oystercard.top_up(max_balance+1) }.to raise_error "limit of #{max_balance} exceeded"
+    end
+
+    it "deducts the fare from the balance" do
+      oystercard.top_up(40)
+      expect{ oystercard.deduct(20) }.to change{ oystercard.balance }.by(-20)
+    end
+
   end
 
-  it "tops up the balance by a specified amount" do
-    expect{ oystercard.top_up(10) }.to change{ oystercard.balance }.by(10)
-  end
+  context "Journey" do
 
-  it "raises an error if balance were to exceed #MAX_BALANCE" do
-    max_balance = described_class::MAX_BALANCE
-    expect{ oystercard.top_up(max_balance+1) }.to raise_error "limit of #{max_balance} exceeded"
-  end
+    it "Touch in will change #in_use to true" do
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
 
-  it "deducts the fare from the balance" do
-    oystercard.top_up(40)
-    expect{ oystercard.deduct(20) }.to change{ oystercard.balance }.by(-20)
+    it "Touch out will change #in_use to false" do
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
+    end
+
   end
 
 end
