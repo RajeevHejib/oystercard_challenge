01ec91915f7963e5187c948b681575f7f2c259f7
alaanzr <alaanzr@gmail.com>
2015-10-21 12:18:36 +0100

    Added Station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,10 +2,12 @@ require 'station'
 
 describe Station do
 
-  subject(:station) { described_class.new('station_name', 1) }
+  subject(:station) { described_class.new('Victoria',  1) }
 
-  it "has a name and zone attribute" do
-    expect(station).to have_attributes(:name => 'station_name', :zone => 1)
+  context '#initialization' do
+    it "has a name and zone attribute" do
+      expect(station).to have_attributes(:name => 'Victoria', :zone => 1)
+    end
   end
 
 end
