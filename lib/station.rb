aedfac097087c98180aca47b68250f6761ff87e1
alaanzr <alaanzr@gmail.com>
2015-10-21 12:31:28 +0100

    Added a hash containing station name/zone key/value pairs

--- lib/station.rb
+++ lib/station.rb
@@ -2,9 +2,12 @@ class Station
 
   attr_reader :name, :zone
 
-  def initialize(name,zone)
+  HASH = { 'Victoria' => 1, 'Euston' => 1, 'Barking' => 4, 'Upminster' => 6 }
+
+  def initialize(name)
+    raise "Invalid station" if HASH[name].nil?
     @name = name
-    @zone = zone
+    @zone = HASH[name]
   end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,12 +2,16 @@ require 'station'
 
 describe Station do
 
-  subject(:station) { described_class.new('Victoria',  1) }
+  subject(:station) { described_class.new('Victoria') }
 
   context '#initialization' do
     it "has a name and zone attribute" do
       expect(station).to have_attributes(:name => 'Victoria', :zone => 1)
     end
+  end
+
+  it "raises an error if an invalid station name is entered" do
+    expect{Station.new('Wimbledon')}.to raise_error "Invalid station"
   end
 
 end
